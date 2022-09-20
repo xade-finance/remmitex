d93f8bd5b6254d0840b49d85a840f778da36c79d
Arav <56185632+arav06@users.noreply.github.com>
2022-09-20 17:56:29 +0530

    Update mobileAPI.py

--- webapp/src/api/mobileAPI.py
+++ webapp/src/api/mobileAPI.py
@@ -1,31 +1,32 @@
 from pymongo import MongoClient
-from flask import Flask,request
+from flask import Flask, request
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
+    try:
+        phone = str(request.args.get('phone'))
+        if phone == None:
+            return "'phone' parameter was not found"
+        client = MongoClient('mongodb://localhost:27017/')
+        database = client['xade']
+        phones = database['phones']
         try:
-                phone = str(request.args.get("phone"))
+            phoneDetails = phones.find_one({'Phone Number': str(phone)})
+            uid = phoneDetails['ID']
+            wallets = database['wallets']
+            try:
+                walletDetails = wallets.find_one({'ID': uid})
+                return walletDetails['Wallet Address']
+            except:
+                return 'Wallet Address was not found'
         except:
-                return "'phone' parameter was not specified"
-        phone = str(request.args.get("phone"))
-        client = MongoClient("mongodb://localhost:27017/")
-        database = client["xade"]
-        phones = database["phones"]
-        try:
-                phoneDetails = phones.find_one({"Phone Number":str(phone)})
-        except:
-                return "Phone Number was not found"
-        phoneDetails = phones.find_one({"Phone Number":str(phone)})
-        uid = phoneDetails["ID"]
-        wallets = database["wallets"]
-        try:
-                walletDetails = wallets.find_one({"id":uid})
-        except:
-                return "Wallet Address was not found"
-        walletDetails = wallets.find_one({"ID":uid})
-        return walletDetails["Wallet Address"]
+            return 'Phone Number was not found'
+    except:
+
+        return "'phone' parameter was not specified"
 
 if __name__ == '__main__':
-        app.run('127.0.0.1',8002)
+    app.run('127.0.0.1', 8002)
