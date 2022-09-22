f30f7f97f6cdeaccb60914aee81b2611b6c230ac
Arav <56185632+arav06@users.noreply.github.com>
2022-09-22 19:08:33 +0530

    added 404 and 200 response codes

--- webapp/src/api/mobileAPI.py
+++ webapp/src/api/mobileAPI.py
@@ -1,32 +1,33 @@
 from pymongo import MongoClient
-from flask import Flask, request
+from flask import Flask,request
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-    try:
-        phone = str(request.args.get('phone'))
-        if phone == None:
-            return "'phone' parameter was not found"
-        client = MongoClient('mongodb://localhost:27017/')
-        database = client['xade']
-        phones = database['phones']
         try:
-            phoneDetails = phones.find_one({'Phone Number': str(phone)})
-            uid = phoneDetails['ID']
-            wallets = database['wallets']
-            try:
-                walletDetails = wallets.find_one({'ID': uid})
-                return walletDetails['Wallet Address']
-            except:
-                return 'Wallet Address was not found'
+                phone = str(request.args.get("phone"))
         except:
-            return 'Phone Number was not found'
-    except:
-
-        return "'phone' parameter was not specified"
+                return "'phone' parameter was not specified",404
+        phone = str(request.args.get("phone"))
+        client = MongoClient("mongodb://localhost:27017/")
+        database = client["xade"]
+        phones = database["phones"]
+        try:
+                phoneDetails = phones.find_one({"Phone Number":str(phone)})
+        except:
+                return "Phone Number was not found",404
+        phoneDetails = phones.find_one({"Phone Number":str(phone)})
+        if phoneDetails == None:
+            return "Phone number was not found",404
+        uid = phoneDetails["ID"]
+        wallets = database["wallets"]
+        try:
+                walletDetails = wallets.find_one({"id":uid})
+        except:
+                return "Wallet Address was not found",404
+        walletDetails = wallets.find_one({"ID":uid})
+        return walletDetails["Wallet Address"],200
 
 if __name__ == '__main__':
-    app.run('127.0.0.1', 8002)
+        app.run('127.0.0.1',8002)
