51c6583edc657660c442276454410862447ff908
Arav <56185632+arav06@users.noreply.github.com>
2022-10-02 18:34:39 +0530

    Delete etherScan.ts

--- webapp/src/services/etherScan.ts
+++
@@ -1,8 +0,0 @@
-export async function getNormalTransactionsByAddress(address: string) {
-    try{
-        const response = await fetch(`https://api-alfajores.celoscan.io/api?module=account&action=txlist&address=${address}&sort=desc&apikey=P74P47ASILQ35VQ3G3UK9BENFR68FXV1VJ`);
-        return await response.json();
-    } catch(error) {
-        return [];
-    }
-} 
