d4a0c64e6dba8823fd61ac9a260a966acbeb7c0a
Arav <56185632+arav06@users.noreply.github.com>
2022-10-08 13:57:32 +0530

    Update CarouselCard3.tsx

--- webapp/src/components/CarouselCard/CarouselCard3.tsx
+++ webapp/src/components/CarouselCard/CarouselCard3.tsx
@@ -5,12 +5,13 @@ type Props = {}
 const CarouselCard3 = (props: Props) => {
   return (
 
-         <div className="box">
-<img src="https://www.xade.finance/media/money.png" />   
+         <div className="box">   
 <br />
 <br />
-<a className="labelText">Beat Inflation with our stable savings account where you can earn upto 9.1% APY</a>
+<a className="labelText"><br />Beat Inflation with our stable savings account where you can earn upto 9.1% APY<br /><br /></a>
 <br />
+<img src="https://www.xade.finance/media/money.png" className="carouselImg" />
+
 <br />
 <br />
 <br />
