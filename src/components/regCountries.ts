d6b68adedc92eaee167c083f992d8c2220d5830a
Arav <56185632+arav06@users.noreply.github.com>
2022-12-20 03:13:58 +0530

    removed email from registration list

--- src/components/regCountries.ts
+++ src/components/regCountries.ts
@@ -14,10 +14,6 @@ const countries: Country[] = [
     "code": "0",
     "name": "Select a country"
     },
-  {
-    "code": "145155141151154",
-    "name": "Email Address"
-    },
     {
       "code": "+7 840",
       "name": "Abkhazia"
